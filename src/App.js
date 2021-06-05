e4af9feedc74f9caef33d8f9c6860e4b00bdc002
Ernesto <ridik_ecp@hotmail.com>
2021-06-05 14:15:21 -0500

    Get questions values in App component

--- src/App.js
+++ src/App.js
@@ -1,47 +1,56 @@
-import Navbar from './components/Navbar';
-import Questionnaire_anx from './components/questionaire_anx';
-import { QuestionsAnx } from './components/Questions'
-import Swal from 'sweetalert2'
-
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Questionnaire_anx from "./components/questionaire_anx";
+import { QuestionsAnx } from "./components/Questions";
+import Swal from "sweetalert2";
 
 function App() {
+  const [values, setvalues] = useState({});
+
+  const getValues = (target, value) => {
+    setvalues((values) => ({ ...values, [target]: value }));
+  };
+
   const hadleCancel = () => {
     Swal.fire({
-      title: '¿Estas seguro de cancelar el test?',
-      icon: 'warning',
+      title: "¿Estas seguro de cancelar el test?",
+      icon: "warning",
       showCancelButton: true,
-      confirmButtonText: 'Si',
-      cancelButtonText: 'No',
-      confirmButtonColor: '#21445F',
-      cancelButtonColor: '#FF3633',
-    })
-  }
+      confirmButtonText: "Si",
+      cancelButtonText: "No",
+      confirmButtonColor: "#21445F",
+      cancelButtonColor: "#FF3633",
+    });
+    // setvalues({});
+  };
   return (
     <>
-      <Navbar/>,
+      <Navbar />,
       <div className="container mb-5">
-        {
-          QuestionsAnx.map( ( question, key ) => (
-            <Questionnaire_anx
-              key={key}
-              number={ key+1 }
-              question={ question }
-            />
-          ))
-        }
+        {QuestionsAnx.map((question, key) => (
+          <Questionnaire_anx
+            key={key}
+            number={key + 1}
+            action={getValues}
+            question={question}
+          />
+        ))}
         <div className="d-flex justify-content-end">
           <button
             type="button"
-            style={{backgroundColor: '#607D8B'}}
+            style={{ backgroundColor: "#607D8B" }}
             className="btn btn-secondary me-3"
             onClick={hadleCancel}
           >
             Cancelar
           </button>
-          <button 
-            type="button" 
-            style={{backgroundColor: '#607D8B'}}  
+          <button
+            type="button"
+            style={{ backgroundColor: "#607D8B" }}
             className="btn btn-secondary"
+            onClick={() => {
+              console.log(values);
+            }}
           >
             Enviar
           </button>
--- src/components/questionaire_anx/Questionnaire_anx.js
+++ src/components/questionaire_anx/Questionnaire_anx.js
@@ -1,82 +1,81 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 export default function Questionnaire_anx(props) {
-    const [optionSelected, setOptionSelected] = useState()
+  const [optionSelected, setOptionSelected] = useState();
 
-    const handleChooise = ( e ) => {
-        console.log( e.target.value )
-        setOptionSelected(e.target.value)
-    }
+  const handleChooise = (e) => {
+    // console.log(e.target.value);
+    setOptionSelected(e.target.value);
+    props.action(props.number, e.target.value);
+  };
 
-    return (
-        <div className="card border-secondary mb-3">
-        <div className="card-header">Pregunta {props.number}</div>
-            <div className="card-body">
-                <p className="card-text">{props.question}
-                </p>
-                <div
-                    className="form-group" style={{ display: 'flex', justifyContent: 'space-around' }}
-                    onChange={ handleChooise }
-                >
-                    <div className="form-check">
-                        <label className="form-check-label">
-                            <input
-                                type="radio"
-                                className="form-check-input"
-                                value='1'
-                                checked={ optionSelected === '1' }
-                            />
-                            <span>1</span>
-                        </label>
-                    </div>
-                    <div className="form-check">
-                        <label className="form-check-label">
-                            <input
-                                type="radio"
-                                className="form-check-input"
-                                value='2'
-                                checked={ optionSelected === '2' }
-                            />
-                            <span>2</span>
-                        </label>
-                    </div>
-                    <div className="form-check">
-                        <label className="form-check-label">
-                            <input
-                                type="radio"
-                                className="form-check-input"
-                                value='3'
-                                checked={ optionSelected === '3' }
-                            />
-                            <span>3</span>
-                        </label>
-                    </div>
-                    <div className="form-check">
-                        <label className="form-check-label">
-                            <input
-                                type="radio"
-                                className="form-check-input"
-                                value='4'
-                                checked={ optionSelected === '4' }
-                            />
-                            <span>4</span>
-                        </label>
-                    </div>
-                    <div className="form-check">
-                        <label className="form-check-label">
-                            <input
-                                type="radio"
-                                className="form-check-input"
-                                value='5'
-                                checked={ optionSelected === '5'}
-                            />
-                            <span>5</span>
-                        </label>
-                    </div>
-                </div>
-                    
-            </div>
+  return (
+    <div className="card border-secondary mb-3">
+      <div className="card-header">Pregunta {props.number}</div>
+      <div className="card-body">
+        <p className="card-text">{props.question}</p>
+        <div
+          className="form-group"
+          style={{ display: "flex", justifyContent: "space-around" }}
+          onChange={handleChooise}
+        >
+          <div className="form-check">
+            <label className="form-check-label">
+              <input
+                type="radio"
+                className="form-check-input"
+                value="1"
+                checked={optionSelected === "1"}
+              />
+              <span>1</span>
+            </label>
+          </div>
+          <div className="form-check">
+            <label className="form-check-label">
+              <input
+                type="radio"
+                className="form-check-input"
+                value="2"
+                checked={optionSelected === "2"}
+              />
+              <span>2</span>
+            </label>
+          </div>
+          <div className="form-check">
+            <label className="form-check-label">
+              <input
+                type="radio"
+                className="form-check-input"
+                value="3"
+                checked={optionSelected === "3"}
+              />
+              <span>3</span>
+            </label>
+          </div>
+          <div className="form-check">
+            <label className="form-check-label">
+              <input
+                type="radio"
+                className="form-check-input"
+                value="4"
+                checked={optionSelected === "4"}
+              />
+              <span>4</span>
+            </label>
+          </div>
+          <div className="form-check">
+            <label className="form-check-label">
+              <input
+                type="radio"
+                className="form-check-input"
+                value="5"
+                checked={optionSelected === "5"}
+              />
+              <span>5</span>
+            </label>
+          </div>
+        </div>
+      </div>
     </div>
-    )
+  );
 }
-
