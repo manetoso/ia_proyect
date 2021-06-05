ab43ae3b7cb82b7712eb582fb6e8ceed8b6e244e
Cristina <17030674@itcelaya.edu.mx>
2021-06-05 13:12:12 -0500

    Questionnaire Anxiety
    The radio buttons works!

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import Navbar from './components/Navbar';
 import Questionnaire_anx from './components/questionaire_anx';
 import { QuestionsAnx } from './components/Questions'
+import Swal from 'sweetalert2'
 
 
 function App() {
+  const hadleCancel = () => {
+    Swal.fire({
+      title: '¿Estas seguro de cancelar el test?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Si',
+      cancelButtonText: 'No',
+      confirmButtonColor: '#21445F',
+      cancelButtonColor: '#FF3633',
+    })
+  }
   return (
     <>
       <Navbar/>,
-      <div className="container">
+      <div className="container mb-5">
         {
           QuestionsAnx.map( ( question, key ) => (
             <Questionnaire_anx
@@ -17,6 +29,23 @@ function App() {
             />
           ))
         }
+        <div className="d-flex justify-content-end">
+          <button
+            type="button"
+            style={{backgroundColor: '#607D8B'}}
+            className="btn btn-secondary me-3"
+            onClick={hadleCancel}
+          >
+            Cancelar
+          </button>
+          <button 
+            type="button" 
+            style={{backgroundColor: '#607D8B'}}  
+            className="btn btn-secondary"
+          >
+            Enviar
+          </button>
+        </div>
       </div>
     </>
   );
